ef668b53f58019e243592f16970105ffbb6e3c49
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-04 08:28:16 +0000

    add select3

--- day2/select3.sql
+++ day2/select3.sql
@@ -2,13 +2,24 @@
 -- SELECT  last_name, first_name 
 -- FROM customers;
 
+-- USE bakery;
+-- SELECT last_name, 
+-- first_name, 
+-- birth_date, 
+-- phone, 
+-- city, 
+-- state,
+-- total_money_spent,
+-- total_money_spent + 100
+-- FROM customers;
+
+-- order of execution: PEMDAS-paranthesis, exponential, multiple,...
+-- USE bakery;
+-- SELECT DISTINCT state
+-- FROM customers;
+
+
+
 USE bakery;
-SELECT last_name, 
-first_name, 
-birth_date, 
-phone, 
-city, 
-state,
-total_money_spent,
-total_money_spent + 100
+SELECT DISTINCT city, state
 FROM customers;
