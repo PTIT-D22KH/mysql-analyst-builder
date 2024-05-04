f351ab83c2a599b6432e3f7486f9f495af1665d3
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-04 08:24:44 +0000

    add select3

---
+++ day2/select3.sql
@@ -0,0 +1,14 @@
+-- USE bakery;
+-- SELECT  last_name, first_name 
+-- FROM customers;
+
+USE bakery;
+SELECT last_name, 
+first_name, 
+birth_date, 
+phone, 
+city, 
+state,
+total_money_spent,
+total_money_spent + 100
+FROM customers;
