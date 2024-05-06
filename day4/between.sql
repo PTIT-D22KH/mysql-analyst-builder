57ee4ceb52e56eddd235926c338cecffdd7617d2
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-06 02:38:55 +0000

    add between operator

---
+++ day4/between.sql
@@ -0,0 +1,14 @@
+USE bakery;
+-- SELECT * FROM customers
+-- WHERE total_money_spent BETWEEN 534 AND 1009;
+
+
+-- SELECT * FROM customers
+-- WHERE birth_date BETWEEN '1989-04-28' AND '1999-02-07';
+
+
+-- SELECT * FROM customers
+-- WHERE first_name BETWEEN 'Kevin' AND 'Obi';
+
+SELECT * FROM customers
+WHERE first_name BETWEEN 'K' AND 'O';
