4deb2fe8fa17c05e9bbcffbfba30fc567416356b
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-07 01:56:35 +0000

    add files

---
+++ day5/orderBy.sql
@@ -0,0 +1,23 @@
+USE bakery;
+-- SELECT * FROM customers
+-- ORDER BY first_name DESC
+-- ;
+
+
+
+-- SELECT * FROM customers
+-- ORDER BY state, total_money_spent
+-- ;
+
+
+
+-- SELECT * FROM customers
+-- ORDER BY state DESC, total_money_spent DESC
+-- ;
+
+
+SELECT * FROM customers
+
+ORDER BY 8 DESC, 9 ASC 
+-- order by 8th column in descending order and 9th column in ascending order
+;
