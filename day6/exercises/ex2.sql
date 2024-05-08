5df1fa657abe8a2bfa7242b6977fcabfe4f80283
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-08 03:20:52 +0000

    rétructure

--- day6/exercises/ex2.sql
+++ day6/exercises/ex2.sql
@@ -1,4 +1,8 @@
 USE day6;
 -- SELECT * FROM shopping_cart;
+SELECT customer_id , ROUND((purchased_items / carted_items * 100),2) AS purchased_percentage
 
+FROM shopping_cart
+ORDER BY customer_id DESC
+;
 
