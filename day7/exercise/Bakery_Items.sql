a9f5a8d089ac50e9be94dd880219451814e946c8
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-09 03:40:09 +0000

    add exercise

---
+++ day7/exercise/Bakery_Items.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE bakery_items (
+    product_name VARCHAR(255)
+);
+
+INSERT INTO bakery_items (product_name) VALUES
+('Double Chocolate Doughnut'),
+('Sweet Loaf'),
+('Croissant'),
+('Chocolate Banana Muffin'),
+('Glazed Doughnut'),
+('Cinnamon Roll'),
+('Cheese Danish'),
+('Peanut Butter Chocolate Pound Cake'),
+('Fruit Tart');
---
+++ day7/exercise/ex1.sql
@@ -0,0 +1,10 @@
+-- USE day7;
+-- SELECT SUBSTRING('Hello World', 1, 4);
+
+USE bakery;
+-- SELECT * FROM customers
+-- WHERE first_name LIKE 'K%' AND first_name LIKE '%n'
+-- WHERE first_name LIKE 'K%%n'
+
+
+SELECT STR_TO_DATE('01,5,2013','%d,%m,%Y');
