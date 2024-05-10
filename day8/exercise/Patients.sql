b34dbe91944bc5d389eab647f0292ae798c72ee3
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-10 09:42:09 +0000

    add exercise day8

---
+++ day8/exercise/Patients.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE patients (
+    patient_id INT,
+    height_inches INT,
+    weight_pounds INT
+);
+
+INSERT INTO patients (patient_id, height_inches, weight_pounds) VALUES
+(1001, 69, 178),
+(1002, 63, 250),
+(1003, 70, 190),
+(1004, 66, 155),
+(1005, 65, 375),
+(1006, 72, 100),
+(1007, 68, 450),
+(1008, 61, 290);
