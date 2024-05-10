b36382f3ab23227dc5cdead74e55d429bb634c1b
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-10 09:26:34 +0000

    add cast convert function

--- day8/cast_convert.sql
+++ day8/cast_convert.sql
@@ -1,1 +1,16 @@
 USE bakery;
+-- SELECT CAST('2022-01-01' AS DATETIME)
+
+
+
+-- SELECT birth_date,
+-- CAST(birth_date AS DATETIME),
+-- CONVERT(birth_date, DATETIME)
+-- FROM customers;
+
+
+--  cannot convert!!!
+SELECT first_name,
+CAST(first_name AS DECIMAL)
+FROM customers
+;
