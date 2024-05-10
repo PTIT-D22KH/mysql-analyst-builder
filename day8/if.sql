e5f37343261e1ad6f91146ed43221ea59167ccd2
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-10 09:22:17 +0000

    edit day8

--- day8/if.sql
+++ day8/if.sql
@@ -1,2 +1,12 @@
 USE bakery;
-SELECT * FROM customers;
+-- IF (condition, condition is true do this, condition is false do this)
+
+-- SELECT tip,
+-- IF (tip > 1, 'Amazing!', 'Cheap....') AS reaction
+-- FROM customer_orders
+-- ;
+
+SELECT order_total, tip,
+IF (tip > 2, order_total * 0.75, order_total * 1.1) AS new_total
+FROM customer_orders
+;
