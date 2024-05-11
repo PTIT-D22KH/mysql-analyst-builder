320548e08c2d3ea01c7516f6ed05f1b551c0219a
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-11 08:37:41 +0000

    add day9

---
+++ day9/aggregate.sql
@@ -0,0 +1,36 @@
+USE bakery;
+-- SELECT customer_id, AVG(tip) AS average_tips
+-- FROM customer_orders
+-- GROUP BY customer_id
+-- ORDER BY average_tips
+-- ;
+
+-- SELECT customer_id, MAX(tip) AS biggest_tips
+-- FROM customer_orders
+-- GROUP BY customer_id
+-- ORDER BY biggest_tips
+-- ;
+
+
+-- SELECT customer_id, MIN(tip) AS smallest_tips
+-- FROM customer_orders
+-- GROUP BY customer_id
+-- ORDER BY smallest_tips
+-- ;
+
+-- SELECT customer_id, COUNT(tip) AS count_of_tips
+-- FROM customer_orders
+-- GROUP BY customer_id
+-- ORDER BY count_of_tips
+-- ;
+
+
+-- SELECT first_name, last_name, COUNT(phone)
+-- FROM customers
+-- GROUP BY first_name, last_name
+-- ;
+
+SELECT product_id, tip,  COUNT(tip), COUNT(DISTINCT tip)
+FROM customer_orders
+GROUP BY product_id, tip
+ORDER BY product_id;
