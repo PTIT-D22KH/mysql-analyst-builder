2e05eb7b40cafa08bc2c1005f918fc00ed77fe31
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-12 08:50:35 +0000

    update db

--- day10/exercise/ex4.sql
+++ day10/exercise/ex4.sql
@@ -1,6 +1,8 @@
 USE day10;
 
-SELECT *, LOWER(email) as lower_email
-FROM emails
--- SELECT COUNT(LOWER(email)) AS total, COUNT(DISTINCT LOWER(email)) AS not_duplicated_email
--- FROM emails;
+-- SELECT *, LOWER(email) as lower_email
+-- FROM emails
+
+
+SELECT COUNT(LOWER(email)) AS total, COUNT(DISTINCT LOWER(email)) AS not_duplicated_email
+FROM emails;
