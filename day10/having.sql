f01ff57950fdfa051a5e88e05cad2dc7afea6d06
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-12 03:27:58 +0000

    add day10

--- day10/having.sql
+++ day10/having.sql
@@ -1,6 +1,30 @@
 USE bakery;
 
-SELECT customer_id
+-- SELECT *
+-- FROM customer_orders
+-- ;
+
+
+
+-- error
+-- SELECT customer_id, SUM(tip) as total_tips
+-- FROM customer_orders
+-- WHERE total_tips > 5
+-- GROUP BY customer_id
+-- ;
+
+
+-- SELECT customer_id, SUM(tip) as total_tips
+-- FROM customer_orders
+-- GROUP BY customer_id
+-- HAVING total_tips > 5
+-- ;
+
+
+
+SELECT customer_id, SUM(order_total) as total_tips
 FROM customer_orders
 GROUP BY customer_id
+HAVING SUM(order_total) > 40
+ORDER by 2
 ;
---
+++ day10/rollup.sql
@@ -0,0 +1,11 @@
+USE bakery;
+-- SELECT customer_id, SUM(tip) as total_tips
+-- FROM customer_orders
+-- GROUP BY customer_id WITH ROLLUP
+-- ;
+
+
+SELECT customer_id, COUNT(tip) as count_tips
+FROM customer_orders
+GROUP BY customer_id WITH ROLLUP
+;
