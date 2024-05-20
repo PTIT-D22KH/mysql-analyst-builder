a0156a82aa55b0d6708046f74032dd1e664b62bf
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-05-20 11:34:25 +0000

    add day18

---
+++ day18/regex.sql
@@ -0,0 +1,36 @@
+USE bakery;
+-- SELECT * 
+-- FROM z_regular_expression
+-- WHERE  email REGEXP '@gmail'
+-- ;
+
+
+-- SELECT email, REGEXP_SUBSTR(email, '@.+')
+-- FROM z_regular_expression
+-- ;
+
+-- SELECT email, REGEXP_SUBSTR(email, '@[a-z]+')
+-- FROM z_regular_expression
+-- ;
+
+
+-- SELECT phone
+-- FROM z_regular_expression
+-- WHERE phone REGEXP '[0-9]{3}.[0-9]{3}.[0-9]{4}'
+-- ;
+
+-- SELECT phone
+-- FROM z_regular_expression
+-- WHERE phone REGEXP '[0-9]{3}-[0-9]{3}-[0-9]{4}'
+-- ;
+
+
+-- SELECT phrase
+-- FROM z_regular_expression
+-- WHERE phrase REGEXP '^SQL'
+-- ;
+
+SELECT phrase
+FROM z_regular_expression
+WHERE phrase REGEXP '.+SQL.+'
+;
---
+++ day18/regexUseCases.sql
@@ -0,0 +1,12 @@
+USE bakery;
+CREATE TABLE `z_regular_expression` (
+    `reg_id` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+    `phone` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `phrase` VARCHAR(150) NOT NULL,
+    PRIMARY KEY(`reg_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+INSERT INTO `z_regular_expression` VALUES(1, '123-456-789', 'sqlisfun@gmail.com', 'I love SQL');
+INSERT INTO `z_regular_expression` VALUES(2, '123\765\7565', 'mysqlpro@apple.com', 'I love MySQL');
+INSERT INTO `z_regular_expression` VALUES(3, '123-846-6546', 'regxpress@yahoo.com', 'SQL is fun');
+INSERT INTO `z_regular_expression` VALUES(4, '123 545 8657', 'sql4life@gmail.com', 'IlikeSQLForReal');
