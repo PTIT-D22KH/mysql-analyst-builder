fc0420b5db83e8229f2091f3990d824184c44842
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-05-21 14:19:49 +0000

    add files

---
+++ day19/create_table.sql
@@ -0,0 +1,2 @@
+USE bakery;
+CREATE TABLE `bakery`.`customer_sweepstakes` (`sweepstake_id` int, `customer_id` int, `phone` text, `birth_date` text, `address` text, `total_money_spent` int, `income` text, `Are you over 18?` text, `favorite_color` text)
