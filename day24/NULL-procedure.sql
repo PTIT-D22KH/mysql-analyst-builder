59c8b8b5246f13be4194a4ca9761143c203a1d5a
tuanduongvu <vucongtuanduong249@gmail.com>
2024-05-27 11:13:17 +0000

    EDIT

--- day24/NULL-procedure.sql
+++ day24/NULL-procedure.sql
@@ -1,5 +1,6 @@
 USE bakery;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS orders_by_product
 CREATE PROCEDURE orders_by_product(p_product_id INT)
 BEGIN
     IF p_product_id IS NULL
@@ -10,5 +11,33 @@ BEGIN
     WHERE product_id = p_product_id;
 END $$
 DELIMITER ;
-CALL orders_by_product(1003);
-CALL orders_by_product(NULL);
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS orders_by_product2;
+CREATE PROCEDURE orders_by_product2(p_product_id INT)
+BEGIN
+    IF p_product_id IS NULL
+    THEN 
+    SELECT *
+    FROM bakery.customer_orders;
+    ELSE
+    SELECT *
+    FROM bakery.customer_orders
+    WHERE product_id = p_product_id;     
+    END IF;
+END $$
+DELIMITER
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS orders_by_product3;
+CREATE PROCEDURE orders_by_product3(p_product_id INT)
+BEGIN
+    SELECT *
+    FROM bakery.customer_orders
+    WHERE product_id = IFNULL(p_product_id, 1001);
+END $$
+DELIMITER
+
+CALL orders_by_product3(1003);
+CALL orders_by_product3(NULL);
