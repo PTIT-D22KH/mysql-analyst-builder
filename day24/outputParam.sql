b67e60bca2276c9641332331bd6aba6541c6f9b0
tuanduongvu <vucongtuanduong249@gmail.com>
2024-05-27 14:11:55 +0000

    add day24

--- day24/outputParam.sql
+++ day24/outputParam.sql
@@ -3,10 +3,19 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS sum_of_orders;
 CREATE PROCEDURE sum_of_orders(p_product_id INT, OUT sum_totals DECIMAL(9, 2))
 BEGIN
+    DECLARE all_totals DECIMAL(9, 2);
+    DECLARE percentage_total DECIMAL(9,2);
     SELECT SUM(order_total) 
     INTO sum_totals
     FROM bakery.customer_orders
     WHERE product_id = p_product_id;
+    SELECT SUM(order_total)
+    INTO all_totals
+    FROM bakery.customer_orders;
+    SET percentage_total = (sum_totals / all_totals * 100);
+    SELECT sum_totals;
+    SELECT all_totals;
+    SELECT percentage_total;
 END $$
 DELIMITER ;
 
